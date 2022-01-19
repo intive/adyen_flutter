50a1bc64e91100880d935f9fd349032ec6548828
johannes.aigner <johannes.aigner@intive.com>
2022-01-19 15:19:06 +0100

    Added log statements for debugging.

--- android/src/main/kotlin/app/adyen/flutter_adyen/FlutterAdyenPlugin.kt
+++ android/src/main/kotlin/app/adyen/flutter_adyen/FlutterAdyenPlugin.kt
@@ -25,7 +25,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.util.UUID
+import java.util.*
 import okhttp3.MediaType
 import okhttp3.RequestBody
 import org.json.JSONObject
